ccfca503ea08fb06f156aaea1ed6cf0f2dfccfbc
nishant-batra <nishantbatra2000@gmail.com>
2021-03-08 15:48:13 +0530

    add input reference in login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.emailRef = React.createRef();
+    this.passRef = React.createRef();
+  }
+  handleFormSubmit = () => {
+    console.log(this.emailRef.current.value, this.passRef.value);
+  };
   render() {
     return (
       <form className="login-form">
         <div className="login-signup-header"> Log In</div>
         <div className="field">
-          <input type="email" required placeholder="Email" />
+          <input
+            type="email"
+            required
+            placeholder="Email"
+            ref={this.emailRef}
+          />
         </div>
         <div className="field">
-          <input type="password" required placeholder="Password" />
+          <input
+            type="password"
+            required
+            placeholder="Password"
+            ref={this.passRef}
+          />
         </div>
         <div className="field">
-          <button>Log In</button>
+          <button type="button" onClick={this.handleFormSubmit}>
+            Log In
+          </button>
         </div>
       </form>
     );
